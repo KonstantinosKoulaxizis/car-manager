b3c9b458a4e0eedf716e6062b0d356e4cb503ad3
konstantinos <kkoulaxizis07@gmail.com>
2021-05-03 23:10:36 +0300

    develop

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
+import React, { useEffect } from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, BackHandler } from 'react-native'
 
 import Navigation from './navigation/Navigation'
 
 export default function App() {
+  const handleBackButton = () => {
+    return true
+  }
+
+  useEffect(() => {
+    // SplashScreen.hide()
+    BackHandler.addEventListener('hardwareBackPress', handleBackButton)
+  }, [])
+
   return (
     <SafeAreaProvider>
       <Navigation />
